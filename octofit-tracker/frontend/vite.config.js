f79c9697384f3fc51fc1a7aff478a434dcb933f1
samu-acn <samuele.cavaliere@accenture.com>
2026-06-02 22:40:01 +0200

    Refactor Vite config to standard React 19 setup

--- octofit-tracker/frontend/vite.config.js
+++ octofit-tracker/frontend/vite.config.js
@@ -1,40 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
-    port: 5173,
-    host: true,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom']
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
+    port: 5173
   }
 })
